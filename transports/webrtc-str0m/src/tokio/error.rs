f4e2b65b644f8f7bab1e147846c5465a8405f021
Doug Anderson444 <douganderson444@gmail.com>
2024-05-12 21:33:38 -0400

    create ListenStream constructor

--- transports/webrtc-str0m/src/tokio/error.rs
+++ transports/webrtc-str0m/src/tokio/error.rs
@@ -1,16 +1,25 @@
 use libp2p_identity::PeerId;
+use std::io::{self, ErrorKind};
 use thiserror::Error;
 
 /// Error in WebRTC.
 #[derive(Error, Debug)]
 pub enum Error {
+    /// Error in the WebRTC transport.
+    #[error("`str0m` error: `{0}`")]
+    WebRtc(#[from] str0m::RtcError),
+
+    /// IO Error.
     #[error("IO error")]
     Io(#[from] std::io::Error),
 
+    #[error("I/O error: `{0}`")]
+    IoError(ErrorKind),
+    /// Authentication error.
     #[error("failed to authenticate peer")]
     Authentication(#[from] libp2p_noise::Error),
 
-    // Authentication errors.
+    // Invalid peer ID.
     #[error("invalid peer ID (expected {expected}, got {got})")]
     InvalidPeerID { expected: PeerId, got: PeerId },
 
--- transports/webrtc-str0m/src/tokio/transport.rs
+++ transports/webrtc-str0m/src/tokio/transport.rs
@@ -2,9 +2,17 @@
 
 use futures::future::BoxFuture;
 use futures::stream::SelectAll;
-
 use futures::Stream;
-use libp2p_core::transport::{TransportError, TransportEvent};
+use if_watch::tokio::IfWatcher;
+use libp2p_core::multiaddr::Protocol;
+use socket2::{Domain, Socket, Type};
+use std::io;
+use std::net::{IpAddr, SocketAddr};
+use std::task::Waker;
+use tokio::net::UdpSocket;
+
+use libp2p_core::transport::{ListenerId, TransportError, TransportEvent};
+use libp2p_core::Multiaddr;
 use libp2p_identity as identity;
 use libp2p_identity::PeerId;
 
@@ -56,7 +64,18 @@ impl libp2p_core::Transport for Transport {
         id: libp2p_core::transport::ListenerId,
         addr: libp2p_core::Multiaddr,
     ) -> Result<(), libp2p_core::transport::TransportError<Self::Error>> {
-        todo!()
+        let addr =
+            parse_webrtc_listen_addr(&addr).ok_or(TransportError::MultiaddrNotSupported(addr))?;
+
+        let (socket, addr) =
+            bind_socket(addr).map_err(|e| TransportError::Other(Error::IoError(e.kind())))?;
+
+        self.listeners.push(
+            ListenStream::new(id, self.config.clone(), socket, addr)
+                .map_err(|e| TransportError::Other(Error::Io(e)))?,
+        );
+
+        Ok(())
     }
 
     fn remove_listener(&mut self, id: libp2p_core::transport::ListenerId) -> bool {
@@ -95,7 +114,74 @@ impl libp2p_core::Transport for Transport {
 }
 
 /// A stream of incoming connections on one or more interfaces.
-struct ListenStream {}
+struct ListenStream {
+    /// The ID of this listener.
+    listener_id: ListenerId,
+
+    /// The socket address that the listening socket is bound to,
+    /// which may be a "wildcard address" like `INADDR_ANY` or `IN6ADDR_ANY`
+    /// when listening on all interfaces for IPv4 respectively IPv6 connections.
+    listen_addr: SocketAddr,
+
+    /// The config which holds this peer's certificate(s).
+    config: Config,
+
+    /// The Socket for this listener.
+    socket: UdpSocket,
+
+    /// Set to `Some` if this listener should close.
+    ///
+    /// Optionally contains a [`TransportEvent::ListenerClosed`] that should be
+    /// reported before the listener's stream is terminated.
+    report_closed: Option<Option<<Self as Stream>::Item>>,
+
+    /// Watcher for network interface changes.
+    /// Reports [`IfEvent`]s for new / deleted ip-addresses when interfaces
+    /// become or stop being available.
+    ///
+    /// `None` if the socket is only listening on a single interface.
+    if_watcher: Option<IfWatcher>,
+
+    /// Pending event to reported.
+    pending_event: Option<<Self as Stream>::Item>,
+
+    /// The stream must be awaken after it has been closed to deliver the last event.
+    close_listener_waker: Option<Waker>,
+}
+impl ListenStream {
+    /// Creates a new [`ListenStream`] with the given listener id, config, and socket.
+    fn new(
+        listener_id: ListenerId,
+        config: Config,
+        socket: UdpSocket,
+        listen_addr: SocketAddr,
+    ) -> io::Result<Self> {
+        let if_watcher;
+        let pending_event;
+        if listen_addr.ip().is_unspecified() {
+            if_watcher = Some(IfWatcher::new()?);
+            pending_event = None;
+        } else {
+            if_watcher = None;
+            let ma = socketaddr_to_multiaddr(&listen_addr, Some(config.fingerprint));
+            pending_event = Some(TransportEvent::NewAddress {
+                listener_id,
+                listen_addr: ma,
+            })
+        }
+
+        Ok(ListenStream {
+            listener_id,
+            listen_addr,
+            config,
+            socket,
+            report_closed: None,
+            if_watcher,
+            pending_event,
+            close_listener_waker: None,
+        })
+    }
+}
 
 impl Stream for ListenStream {
     type Item = TransportEvent<<Transport as libp2p_core::Transport>::ListenerUpgrade, Error>;
@@ -128,4 +214,68 @@ impl Config {
             id_keys,
         }
     }
+}
+
+/// Turns an IP address and port into the corresponding WebRTC multiaddr.
+fn socketaddr_to_multiaddr(socket_addr: &SocketAddr, certhash: Option<Fingerprint>) -> Multiaddr {
+    let addr = Multiaddr::empty()
+        .with(socket_addr.ip().into())
+        .with(Protocol::Udp(socket_addr.port()))
+        .with(Protocol::WebRTCDirect);
+
+    if let Some(fp) = certhash {
+        return addr.with(Protocol::Certhash(fp.to_multihash()));
+    }
+
+    addr
+}
+/// Parse the given [`Multiaddr`] into a [`SocketAddr`] for listening.
+fn parse_webrtc_listen_addr(addr: &Multiaddr) -> Option<SocketAddr> {
+    let mut iter = addr.iter();
+
+    let ip = match iter.next()? {
+        Protocol::Ip4(ip) => IpAddr::from(ip),
+        Protocol::Ip6(ip) => IpAddr::from(ip),
+        _ => return None,
+    };
+
+    let Protocol::Udp(port) = iter.next()? else {
+        return None;
+    };
+    let Protocol::WebRTCDirect = iter.next()? else {
+        return None;
+    };
+
+    if iter.next().is_some() {
+        return None;
+    }
+
+    Some(SocketAddr::new(ip, port))
+}
+
+/// Create and bind a socket address using the given [`SocketAddr`].
+fn bind_socket(addr: SocketAddr) -> Result<(UdpSocket, SocketAddr), std::io::Error> {
+    let socket = match addr.is_ipv4() {
+        true => {
+            let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(socket2::Protocol::UDP))?;
+
+            socket.bind(&addr.into())?;
+            socket
+        }
+        false => {
+            let socket = Socket::new(Domain::IPV6, Type::DGRAM, Some(socket2::Protocol::UDP))?;
+            socket.set_only_v6(true)?;
+            socket.bind(&addr.into())?;
+            socket
+        }
+    };
+
+    socket.set_reuse_address(true)?;
+    socket.set_nonblocking(true)?;
+    #[cfg(unix)]
+    socket.set_reuse_port(true)?;
+
+    let socket = UdpSocket::from_std(socket.into())?;
+    let local_address = socket.local_addr()?;
+    Ok((socket, local_address))
 }
