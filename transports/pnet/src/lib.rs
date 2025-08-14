6e85234a3e8fe5da578186b434250053f3a71cbd
Charlie Little <26636186-charlie.42@users.noreply.gitlab.com>
2025-08-14 10:53:51 -0500

    impl Deref for PreSharedKey

--- transports/pnet/src/lib.rs
+++ transports/pnet/src/lib.rs
@@ -46,6 +46,7 @@ use salsa20::{
     Salsa20, XSalsa20,
 };
 use sha3::{digest::ExtendableOutput, Shake128};
+use std::ops::Deref;
 
 const KEY_SIZE: usize = 32;
 const NONCE_SIZE: usize = 24;
@@ -55,6 +56,14 @@ const FINGERPRINT_SIZE: usize = 16;
 /// A pre-shared key, consisting of 32 bytes of random data.
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub struct PreSharedKey([u8; KEY_SIZE]);
+
+impl Deref for PreSharedKey {
+    type Target = [u8; KEY_SIZE];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 impl PreSharedKey {
     /// Create a new pre shared key from raw bytes
